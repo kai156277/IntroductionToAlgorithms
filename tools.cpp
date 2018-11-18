68bcfaf3078aaf350a1392ce7ac411082830fb5a
zhao-homePC <1005324930@qq.com>
2018-11-18 19:10:11 +0800

    feat: add Ex:2.1-4

--- tools.cpp
+++ tools.cpp
@@ -4,9 +4,17 @@
 
 void printIntArray(int *array, int len)
 {
-    printf_s("Array(%d): ");
+    printf_s("Array(%d): ", len);
     for(int i = 0; i < len; ++i)
         printf_s("%d ", array[i]);
     
+    printf_s("\n");
+}
+void printIntVector(const std::vector<int> &array)
+{
+    printf_s("Array(%zd): ", array.size());
+    for(int i = 0; i < array.size(); ++i)
+        printf_s("%d ", array[i]);
+
     printf_s("\n");
 }
--- tools.h
+++ tools.h
@@ -1,6 +1,9 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#include <vector>
+
 void printIntArray(int *array, int len);
 
+void printIntVector(const std::vector<int>& array);
 #endif // TOOLS_H
